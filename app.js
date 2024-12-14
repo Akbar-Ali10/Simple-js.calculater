85552f9eacff77a95dff929465763d89d04b44a9
Ali <baltimuhammad38@gmail.com>
2024-12-14 16:14:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+// function Calculator() {
+//     let num1 = document.getElementById("firstName").value;
+//     let num2 = document.getElementById("secondName").value;
+//     let select = document.getElementById("select").value;
+//     if (select == "+") {
+//         var res = parseInt(num1) + parseInt(num2);
+//     } else if (select == "-") {
+//         var res = parseInt(num1) - parseInt(num2);
+//     } else if (select == "*") {
+//         var res = parseInt(num1) * parseInt(num2);
+//     } else if (select == "/") {
+//         var res = parseInt(num1) / parseInt(num2);
+//     }
+//     document.getElementById("result").value = res;
+// }
+
+
+function Calculator() {
+   let num1 = document.getElementById("firstName").value;
+   let num2 = document.getElementById("secondName").value;
+   let select = document.getElementById("select").value;
+
+   if (select = "+") {
+      var res = parseInt(num1) + parseInt(num2);
+   } else if (select = "-") {
+      var res = parseInt(num1) - parseInt(num2);
+   } else if (select = "*") {
+      var res = parseInt(num1) * parseInt(num2);
+   } else if (select = "/") {
+      var res = parseInt(num1) / parseInt(num2);
+   }
+   document.getElementById("result").value = res;
+
+}
+
+
+// function Calculator() {
+//    let num1 = document.getElementById("firstName").value;
+//    let num2 = document.getElementById("secondName").value;
+//    let select = document.getElementById("select").value;
+
+//    if (select = "+") {
+//       var res = parseInt(num1) + parseInt(num2);
+//    }else if (select = "-") {
+//       var res = parseInt(num1) - parseInt(num2);
+//    }else if (select = "*") {
+//       var res = parseInt(num1) * parseInt(num2);
+//    }else if (select = "/") {
+//       var res = parseInt(num1) / parseInt(num2);
+//    }
+//    document.getElementById("result").value = res;
+// }
